ae48b159045c9d69d51be602e48aa43fc450fdf8
Merina Sofiati <dkaky2008@gmail.com>
2024-07-12 06:44:31 +0700

    time

--- Hackathon/src/Components/Home.jsx
+++ Hackathon/src/Components/Home.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Home() {
+  const [currentTime, setCurrentTime] = useState(new Date());
 
-  const n =  new Date();
-  const d = n.getDay();
-  const [day, setDay] = useState(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Ahad' ]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const d = currentTime.getDay();
+  const h = currentTime.getHours();
+  const min = currentTime.getMinutes();
+  const [day] = useState(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Ahad' ]);
 
   return (
     <>
     <h1 style={{textAlign: 'center'}}>Home</h1>
-    <div style={{width: '300px', textAlign: 'left'}}>
-    <p>{day[d - 1]}</p>
+    <div style={{width: '500px', textAlign: 'left'}}>
+    <p>{day[d - 1]} {h}:{min}</p>
     </div>
     </>
   )
--- Hackathon/src/Components/Routine.jsx
+++ Hackathon/src/Components/Routine.jsx
@@ -1,19 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../Config/Firebase';
 
 function Routine() {
-
-  const n =  new Date();
-  const d = n.getDay();
+  const [data, setData] = useState([]);
   const [day, setDay] = useState(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Ahad' ]);
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const d = currentTime.getDay();
+  const h = currentTime.getHours();
+  const min = currentTime.getMinutes();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "Routine"));
+      const dataArray = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setData(dataArray);
+      console.log(dataArray);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
-    <h1 style={{textAlign: 'center'}}>Routine</h1>
-    <div style={{width: '300px', textAlign: 'left'}}>
-    <p>{day[d - 1]}</p>
-    </div>
+      <h1 style={{ textAlign: 'center' }}>Routine</h1>
+      <div style={{ width: '500px', textAlign: 'left' }}>
+        <p>{day[d - 1]} {h}:{min}</p>
+      </div>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default Routine
+export default Routine;
