3a010368e17f5b029188763d01dce15fa9cf01ef
Merina Sofiati <dkaky2008@gmail.com>
2024-07-12 06:19:57 +0700

    list

--- Hackathon/src/Components/Routine.jsx
+++ Hackathon/src/Components/Routine.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Routine() {
+
+  const n =  new Date();
+  const d = n.getDay();
+  const [day, setDay] = useState(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Ahad' ]);
+
   return (
     <>
-    <p>Routine</p>
+    <h1 style={{textAlign: 'center'}}>Routine</h1>
+    <div style={{width: '300px', textAlign: 'left'}}>
+    <p>{day[d - 1]}</p>
+    </div>
     </>
   )
 }
