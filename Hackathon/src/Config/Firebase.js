716e20ac3ee6082d785bd5952bfcd61deace011d
Merina Sofiati <dkaky2008@gmail.com>
2024-07-11 19:55:46 +0700

    firebase connect

---
+++ Hackathon/src/Config/Firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBxWbMwPGod-qtFbycJXCdLVaTBDdNTiJk",
+  authDomain: "hackathon-46b95.firebaseapp.com",
+  projectId: "hackathon-46b95",
+  storageBucket: "hackathon-46b95.appspot.com",
+  messagingSenderId: "453519413680",
+  appId: "1:453519413680:web:5cbb7ff1f9f6e7193e0675",
+  measurementId: "G-CH6QL3EXKM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
