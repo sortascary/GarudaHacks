4f04a348f7d494bf4a469f9eb4b72d2ce5f21028
Merina Sofiati <dkaky2008@gmail.com>
2024-07-11 20:10:41 +0700

    working

--- Hackathon/src/App.jsx
+++ Hackathon/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Login from './Components/Login'
 
 function App() {
 
   return (
     <>
-      <p>hello</p>
+      < Login/>
     </>
   )
 }
