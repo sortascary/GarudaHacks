c4500e567f5578ae12e2dee51ff78a34ac297484
Merina Sofiati <dkaky2008@gmail.com>
2024-07-12 09:29:11 +0700

    yo kiryu

--- Hackathon/src/Components/Chat.jsx
+++ Hackathon/src/Components/Chat.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 function Chat() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const d = currentTime.getDay();
+  let h = currentTime.getHours();
+  const min = currentTime.getMinutes();
+  const ampm = h >= 12 ? 'PM' : 'AM';
+  h = h % 12 || 12;
+  const [day] = useState(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Ahad' ]);
+
   return (
     <>
-    <p>Chat</p>
+    <div style={{width: '100svh', textAlign: 'left'}}>
+    <p style={{ textAlign: 'right' }}>{day[d - 1]} {h}:{min < 10 ? `0${min}` : min} {ampm}</p>
+    </div>
     </>
   )
 }
